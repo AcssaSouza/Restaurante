e6003145421668542b2a17e77b9aa9abe589f83b
AcssaSouza <acssasouza109@gmail.com>
2023-07-10 19:22:46 -0300

    Teste endpoint cardapio

---
+++ source/WebAPI/Restaurante.API/Controllers/CardapioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante.API.Domain;
+
+namespace Restaurante.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class CardapioController : ControllerBase
+    {
+        [HttpGet("Cardapios")]
+        public List<Cardapio> Get()
+        {
+            var listaCardapios = new List<Cardapio>();
+
+            var cardapio = new Cardapio();
+
+            cardapio.CardapioId = 1;
+            cardapio.Nome = "Pizza";
+            cardapio.Preço = 2;
+            cardapio.Ingredientes = "Queijo e Azeitona";
+
+            listaCardapios.Add(cardapio);
+
+            return listaCardapios;
+        }
+    }
+}
---
+++ source/WebAPI/Restaurante.API/Domain/Cardapio.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurante.API.Domain
+{
+    public class Cardapio
+    {
+        public int CardapioId { get; set; }
+        public string Nome { get; set; }
+        public double Preço { get; set; }
+        public string Ingredientes { get; set; }
+
+    }
+}
